0e0b58599b3ec46c0d2105ef6f1373651154c7cd
Basar Buyukkahraman <basarbk@yahoo.com>
2022-06-20 22:51:48 +0300

    Save user to db

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,12 +1,19 @@
 package com.example.demo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+  @Autowired
+  UserRepository userRepository;
+
   @PostMapping("/users")
-  void createUser(){}
+  void createUser(@RequestBody User user){
+    userRepository.save(user);
+  }
   
 }
--- src/test/java/com/example/demo/UserRegistrationTest.java
+++ src/test/java/com/example/demo/UserRegistrationTest.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.demo.user.User;
+import com.example.demo.user.UserRepository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,9 @@ public class UserRegistrationTest {
 
   @Autowired
   TestRestTemplate testRestTemplate;
+
+  @Autowired
+  UserRepository userRepository;
   
   @Test
   public void postUser_whenUserIsValid_returns200(){
@@ -25,5 +29,15 @@ public class UserRegistrationTest {
     user.setPassword("P4ssword");
     ResponseEntity<Object> response = testRestTemplate.postForEntity("/users", user, Object.class);
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+  }
+
+  @Test
+  public void postUser_whenUserIsValid_savesUserToDB(){
+    User user = new User();
+    user.setUsername("user1");
+    user.setEmail("user@mail.com");
+    user.setPassword("P4ssword");
+    testRestTemplate.postForEntity("/users", user, Object.class);
+    assertThat(userRepository.count()).isEqualTo(1);
   }
 }
